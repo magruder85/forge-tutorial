a473d5d60519177de2d80d022007ae2c6ed6aa24
magruder85 <mmagruder@gmail.com>
2017-02-23 20:20:35 -0500

    Added smelting and shaped cooking recipes

--- src/main/java/net/mdment/tutorial/recipe/ModRecipes.java
+++ src/main/java/net/mdment/tutorial/recipe/ModRecipes.java
@@ -1,13 +1,19 @@
 package net.mdment.tutorial.recipe;
 
 import net.mdment.tutorial.item.ModItems;
+import net.mdment.tutorial.block.ModBlocks;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 
 public class ModRecipes {
 	
 	public static void init() {
 		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.cornSeed, 10), ModItems.corn);
+		GameRegistry.addShapedRecipe(new ItemStack(Items.RABBIT_STEW), " R ", "CPM", " B ", 'R', Items.COOKED_RABBIT, 'C', ModItems.corn, 'P', Items.BAKED_POTATO, 'M', Blocks.BROWN_MUSHROOM, 'B', Items.BOWL);
+		GameRegistry.addSmelting(ModBlocks.oreCopper, new ItemStack(ModItems.ingotCopper), 0.7f);
+
 	}
 
 }
